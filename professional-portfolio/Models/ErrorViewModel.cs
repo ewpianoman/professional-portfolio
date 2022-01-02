bbd9070ba2e0029b4f736d8b1c9323d647ae0674
Eric Wilson <ewpiano@gmail.com>
2022-01-01 19:38:00 -0600

    Initial commit - ASP.net MVC scaffold.

---
+++ professional-portfolio/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace professional_portfolio.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
