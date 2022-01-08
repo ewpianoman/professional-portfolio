791bf37250773234c0b734a99e04ad6ad712dd91
Eric Wilson <ewpiano@gmail.com>
2022-01-07 19:46:07 -0600

    Updated PortfolioItem Model.

--- professional-portfolio/Models/PortfolioItem.cs
+++ professional-portfolio/Models/PortfolioItem.cs
@@ -7,9 +7,19 @@ namespace professional_portfolio.Models
         public int Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
-        public string[] ImageURLs { get; set; }
         public bool Featured { get; set; }
         public Image[] Images { get; set; }
+        public PortfolioItem[] RelatedProjects { get; set; }
+
+        // title-cover.jpg
+        public string CoverImageFileName
+        {
+            get
+            {
+                return Title.Replace(" ", "-")
+                    .ToLower() + "-cover.jpg";
+            }
+        }
 
         public PortfolioItem()
         {
