7dd6b14cb29841d9ab85547505ba40dde836a526
Eric Wilson <ewpiano@gmail.com>
2022-01-07 20:20:57 -0600

    Updated Portfolio view.

--- professional-portfolio/Controllers/PortfolioController.cs
+++ professional-portfolio/Controllers/PortfolioController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using professional_portfolio.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -13,7 +14,14 @@ namespace professional_portfolio.Controllers
         // GET: /<controller>/
         public IActionResult Index()
         {
-            return View();
+            var portfolioItem = new PortfolioItem()
+            {
+                Title = "School of Piano Improv",
+                Category = "Website",
+                Description = "A website built on WordPress using the Divi Theme."
+            };
+
+            return View(portfolioItem);
         }
     }
 }
--- professional-portfolio/Models/PortfolioItem.cs
+++ professional-portfolio/Models/PortfolioItem.cs
@@ -6,8 +6,10 @@ namespace professional_portfolio.Models
     {
         public int Id { get; set; }
         public string Title { get; set; }
+        public string Category { get; set; }
         public string Description { get; set; }
         public bool Featured { get; set; }
+        public string Url { get; set; }
         public Image[] Images { get; set; }
         public PortfolioItem[] RelatedProjects { get; set; }
 
