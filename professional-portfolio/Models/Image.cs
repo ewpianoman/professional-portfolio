2e5b74c3d84e0ed7c1eb00b8978358cd5cc23333
Eric Wilson <ewpiano@gmail.com>
2022-01-07 19:33:22 -0600

    Added Portfolio Model, View, & Controller.

---
+++ professional-portfolio/Models/Image.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace professional_portfolio.Models
+{
+    public class Image
+    {
+        public int Id { get; set; }
+        public string FilePath { get; set; }
+        public string Title { get; set; }
+        public string Alt { get; set; }
+
+        public Image()
+        {
+        }
+    }
+}
